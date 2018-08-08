fe7eb3b3c06692e7c2b8951aebc5ee415b9fcb59
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-08-08 12:29:07 +0200

    DEBUG Add debugbreaks

--- drivers/storage/ide/uniata/id_ata.cpp
+++ drivers/storage/ide/uniata/id_ata.cpp
@@ -10537,7 +10537,7 @@ DriverEntry(
 #ifndef USE_REACTOS_DDK
     NTSTATUS               status;
 #endif
-
+    __debugbreak();
     PCONFIGURATION_INFORMATION GlobalConfig = IoGetConfigurationInformation();
     BOOLEAN PrimaryClaimed   = FALSE;
     BOOLEAN SecondaryClaimed = FALSE;
